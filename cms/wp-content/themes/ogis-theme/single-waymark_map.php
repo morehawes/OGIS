e71e2bb1288642642177aa9abce9dde70a234b97
Joe Hawes <morehawes@gmail.com>
2022-11-24 13:06:55 -0800

    2 - Messages

--- cms/wp-content/themes/ogis-theme/single-waymark_map.php
+++ cms/wp-content/themes/ogis-theme/single-waymark_map.php
@@ -2,7 +2,12 @@
 			<div id="waymark">
 				<?php echo do_shortcode('[Waymark map_id="' . get_the_ID() . '" map_hash="ogis_demo_view"]'); ?>
 			</div>
+			
 			<article id="words">
+				<header id="header">
+					<div class="waymark-message waymark-success">Your submission has been published <a href="<?php echo get_permalink(get_the_ID()); ?>">here</a>: <input type="text" value="<?php echo get_permalink(get_the_ID()); ?>" /></div>
+				</header>
+				
 				<h1><?php the_title(); ?></h1>
 			
 				<?php the_content(); ?>
