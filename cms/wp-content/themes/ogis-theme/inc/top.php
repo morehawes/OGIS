aebaca24419ea5ebf8e0faa7705fd8dc5dd21acc
Joe Hawes <morehawes@gmail.com>
2022-11-26 07:58:43 -0800

    Wording tweak

--- cms/wp-content/themes/ogis-theme/inc/top.php
+++ cms/wp-content/themes/ogis-theme/inc/top.php
@@ -4,7 +4,7 @@
 		<meta charset="<?php bloginfo('charset'); ?>" />
 		<meta name="viewport" content="width=device-width" />
 		
-		<title>OGIS | An Open Geospatial Information System</title>
+		<title>OGIS | An Open Geographical Information System</title>
 		
 		<?php wp_head(); ?>
 	</head>
